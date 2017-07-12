922d1a6025631b1a7aaac3c9a4e20ff11933286b
Vojtěch Stoklasa <stoklasavojtech@live.com>
2017-07-12 16:17:33 +0200

    Changed GoogleApi requests into one class, clearer code, started to work on NLP solution by google

--- ConsoleApp1/Google.cs
+++ ConsoleApp1/Google.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Threading;
+
+//using Google.Apis.Services;
+//using Google.Apis.Auth.OAuth2;
+//using Google.Apis.Oauth2.v2;
+
+//using Google.Cloud.Vision.V1;
+//using Google.Cloud.Language.V1;
 
 
-using Google.Cloud.Vision.V1;
-using Google.Apis.Services;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Oauth2.v2;
-using Newtonsoft.Json;
 using TextRecognition.Factory;
 using TextRecognition.DirectoryObject;
+using TextRecognition.GoogleQueries;
+using Google.Cloud.Vision.V1;
+using Google.Apis.Auth.OAuth2;
 
 namespace GoogleApi
 {
@@ -21,19 +26,17 @@ namespace GoogleApi
 
         static void Main(string[] args) {
 
+            Console.WriteLine();
+
             FileFactory fact = new FileFactory();
-
             
-
             DirectoryObject files = new DirectoryObject(
                 Directory.GetFiles(fact.GetImageFolder())
 
             );
-
-            
             try
             {
-                new TextRecognition().Run().Wait();
+                Queries.Run().Wait();
             }
 
             catch (AggregateException ex)
@@ -52,7 +55,7 @@ namespace GoogleApi
                   = Path.GetFileName(files.GetFileInfo(i));
                 string CurrentFilepath = files.GetFileInfo(i);
 
-                
+
                 Console.WriteLine(CurrentFilename);
                 try
                 {
@@ -62,67 +65,19 @@ namespace GoogleApi
                     fact.SetTextPath(CurrentFilename);
                     fact.SaveFile(results);
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
-                    Console.WriteLine("IS_NOT_IMAGE: "+e.StackTrace);
+                    Console.WriteLine("IS_NOT_IMAGE: " + e.StackTrace);
                     continue;
                 }
                 
-                
-
                 Console.WriteLine("END_OF_API_REQUEST\nOutput file saved in: " + fact.GetOutputPath());
-               
+
             }
             Console.WriteLine("OCR done.");
         }
 
-        public static List<string> GoogleVisionQuery(Image img)
-        {
-            List<string> data = new List<string>();
-
-            var image = img;
-
-            var client = ImageAnnotatorClient.Create();
-
-            var response = client.DetectText(image);
-
-            
-            foreach (var annotation in response)
-            {
-                if ((null != annotation.Description))
-                {
-                    if (annotation.Description.Contains("\n"))
-                    {
-
-                        data.Add(annotation.Description);
-                    }
-
-                    else if (!annotation.Description.Contains("\n"))
-                    {
-                        Console.WriteLine(annotation.Description);
-                    }
-                }
-            }
-                
-            return data;
-        }
-        
-        public async Task Run()
-        {
-            string CRED_PATH = @"C:\Users\vojtech.stoklasa\Documents\Visual Studio 2017\Projects\TextRecognition\ConsoleApp1\bin\Debug\api_key\My First Project-ca909aeb1219.json";
-
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", CRED_PATH);
-
-            GoogleCredential credential = await GoogleCredential.GetApplicationDefaultAsync();
-
-            var service = new Oauth2Service(new BaseClientService.Initializer(){
-
-                    HttpClientInitializer = credential,
-                    ApplicationName = "Oauth2 Sample",
-                }
-            );
-
-        }
+       
       
     }
 }
---
+++ ConsoleApp1/Queries.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Google.Apis.Services;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Oauth2.v2;
+
+using Google.Cloud.Vision.V1;
+using Google.Cloud.Language.V1;
+
+namespace TextRecognition.GoogleQueries
+{
+    class Queries
+    {
+
+        public static async Task Run()
+        {
+            string CRED_PATH = @"C:\Users\vojtech.stoklasa\Documents\Visual Studio 2017\Projects\TextRecognition\ConsoleApp1\bin\Debug\api_key\My First Project-ca909aeb1219.json";
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", CRED_PATH);
+
+            GoogleCredential credential = await GoogleCredential.GetApplicationDefaultAsync();
+
+            var service = new Oauth2Service(new BaseClientService.Initializer(){
+
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Oauth2 Sample",
+                }
+            );
+
+        }
+
+        public static void NLPQuery(string text)
+        {
+            var client = LanguageServiceClient.Create();
+            var  response = client.AnalyzeEntities(new Document()
+            {
+                Content = text,
+                Type = Document.Types.Type.PlainText
+            });
+            foreach(var entity in response)
+            {
+
+            }
+
+
+        }
+
+        public static List<string> GoogleVisionQuery(Image img)
+        {
+            List<string> data = new List<string>();
+
+            var image = img;
+
+            var client = ImageAnnotatorClient.Create();
+
+            var response = client.DetectText(image);
+
+
+            foreach (var annotation in response)
+            {
+                if ((null != annotation.Description))
+                {
+                    if (annotation.Description.Contains("\n"))
+                    {
+
+                        data.Add(annotation.Description);
+                    }
+
+                    else if (!annotation.Description.Contains("\n"))
+                    {
+                        Console.WriteLine(annotation.Description);
+                    }
+                }
+            }
+
+            return data;
+        }
+    }
+}
